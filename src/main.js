f996eb979281ea80e7d7f3349303ec826e435c40
Nick Hennessy <njhnny@gmail.com>
2020-11-20 16:24:24 -0800

    call api and log response

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+fetch('https://v6.exchangerate-api.com/v6/1634eff7eeeb331eae57c486/latest/USD')
+  .then( response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  });
