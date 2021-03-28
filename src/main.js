d3e04581ba9a99c617d414dcd1192138e932b96f
Nick Hennessy <njhnny@gmail.com>
2021-03-28 01:26:53 -0700

    replace fetch code

--- src/main.js
+++ src/main.js
@@ -2,19 +2,23 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-$(document).ready(function() {
-  $("form#form").submit(function(event) {
-    event.preventDefault();
-    let amount = $("#amount").val();
-    console.log(amount);
-    let currency = $("#currency").val();
-    console.log(currency);
-    fetch('https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD')
-      .then( response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      });
-  });
+
+$("form#form").submit(function(event) {
+  event.preventDefault();
+  let amount = $("#amount").val();
+  console.log(amount);
+  let currency = $("#currency").val();
+  console.log(currency);
+  fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      console.log(response.json);
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    });
 });
+
