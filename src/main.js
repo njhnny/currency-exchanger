3b26d32f9735ff04102ff97ec42cb9926aba4401
Nick Hennessy <njhnny@gmail.com>
2021-03-28 02:40:57 -0700

    remove api call

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeService from './exchange.js';
+
+
+
+
 
 $("form#form").submit(function(event) {
   event.preventDefault();
@@ -9,16 +14,8 @@ $("form#form").submit(function(event) {
   console.log(amount);
   let currency = $("#currency").val();
   console.log(currency);
-  fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    .then(function(response) {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      console.log(response.json);
-      return response.json();
-    })
-    .catch(function(error) {
-      return error;
-    });
+  const response = ExchangeService.getExchange();
+  console.log(response);
+  $('.output').text(response);
 });
 
