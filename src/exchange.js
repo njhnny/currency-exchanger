aa7d911849e61c62c643e0fd3c0edb1f374e4f54
Nick Hennessy <njhnny@gmail.com>
2021-03-28 01:57:44 -0700

    add exchange.js

---
+++ src/exchange.js
@@ -0,0 +1,14 @@
+export default class ExchangeService {  
+  static getExchange(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      })
+  }
+}
